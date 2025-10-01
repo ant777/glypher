e3af90b59c8bf13b090234c210b5acba99ff4632
ant777 <ant777@tut.by>
2025-10-01 16:38:27 +0300

    upd

--- sw.sh
+++ sw.sh
@@ -5,10 +5,10 @@
 
 FOLDER="./docs"
 
-if [[ -z "$FOLDER" ]]; then
-  echo "Usage: $0 /path/to/folder"
-  exit 1
-fi
+# if [[ -z "$FOLDER" ]]; then
+#   echo "Usage: $0 /path/to/folder"
+#   exit 1
+# fi
 
 # Get list of file names excluding .map and .cache
 FILE_LIST=$(find "$FOLDER" -type f ! -name "*.map*" ! -name "*.cache*" -printf "%P\n")
@@ -20,10 +20,10 @@ JS_ARRAY="[\n'/glypher/',$JS_ARRAY\n]"
 # Find the target file (excluding .map files)
 TARGET_FILE=$(find "$FOLDER" -type f -name "*service-worker*" ! -name "*.map*" | head -n 1)
 
-if [[ -z "$TARGET_FILE" ]]; then
-  echo "No matching service-worker file found."
-  exit 1
-fi
+# if [[ -z "$TARGET_FILE" ]]; then
+#   echo "No matching service-worker file found."
+#   exit 1
+# fi
 
 # Replace $NAMES$ in the target file
 sed -i "s|\[\]|$JS_ARRAY|" "$TARGET_FILE"
